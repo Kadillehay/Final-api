cee0850f86935cd22f9373d378a568f421f735bc
Karen Dillehay <117693236+Kadillehay@users.noreply.github.com>
2023-10-20 09:13:56 -0500

    before pulling ogulcan's code

--- final/src/main/java/com/coderscampus/api/web/DashboardController.java
+++ final/src/main/java/com/coderscampus/api/web/DashboardController.java
@@ -14,24 +14,24 @@ import com.coderscampus.api.domain.User;
 import com.coderscampus.api.models.UserModel;
 import com.coderscampus.api.repository.FarmRegisterRepository;
 
-@RestController
-@CrossOrigin(origins = "*")
-public class DashboardController {
-	
-	@Autowired
-	FarmRegisterRepository farmRegisterRepo;
-	@Autowired
-	UserModel userModel;
+//@RestController
+//@CrossOrigin(origins = "*")
+//public class DashboardController {
+//	
+//	@Autowired
+//	FarmRegisterRepository farmRegisterRepo;
+//	@Autowired
+//	UserModel userModel;
 	
 	//Ogul I just copied this from the register, I assume the logic is the same?
-	@PostMapping("/user-dashboard")
-	public ResponseEntity<List<Object>> updateRegister(@RequestBody User farmRegistry) {
-		User user = new User();
-		BeanUtils.copyProperties(farmRegistry, user);
-		
-		User registered = farmRegisterRepo.save(user);
-		System.out.println("working?");
-		return ResponseEntity.ok(List.of(registered.getId(), registered.getFarmName()));
-	
-	}
-}
+//	@PostMapping("/user-dashboard")
+//	public ResponseEntity<List<Object>> updateRegister(@RequestBody User farmRegistry) {
+//		User user = new User();
+//		BeanUtils.copyProperties(farmRegistry, user);
+//		
+//		User registered = farmRegisterRepo.save(user);
+//		System.out.println("working?");
+//		return ResponseEntity.ok(List.of(registered.getId(), registered.getFarmName()));
+//	
+//	}
+//}
